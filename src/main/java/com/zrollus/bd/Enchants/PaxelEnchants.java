05d65bc0c211a93d01a6335b751cdb9c14d8de09
Amy <MineGuy0.swappes@gmail.com>
2025-11-06 11:44:51 +0100

    nerfed items

---
+++ src/main/java/com/zrollus/bd/Enchants/PaxelEnchants.java
@@ -0,0 +1,46 @@
+package com.zrollus.bd.Enchants;
+import com.zrollus.bd.item.Custom.HammerItem;
+import com.zrollus.bd.item.Custom.PaxelItem;
+import com.zrollus.bd.item.Custom.ReaperItem;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentTarget;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.PickaxeItem;
+
+public class ArthropedicEfficiencyEnchantment extends Enchantment {
+    public ArthropedicEfficiencyEnchantment() {
+        super(Rarity.UNCOMMON, EnchantmentTarget.DIGGER, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 1;
+    }
+
+    @Override
+    public boolean isAcceptableItem(ItemStack stack) {
+        if (stack.getItem() instanceof HammerItem) {
+            return false;
+        }
+        if (stack.getItem() instanceof PaxelItem) {
+            return false;
+        }
+        if (stack.getItem() instanceof ReaperItem) {
+            return true;
+        }
+
+        // Otherwise, reject:
+        return false;
+    }
+    @Override
+    public boolean isAvailableForEnchantedBookOffer() {
+        return true;
+    }
+
+    @Override
+    public boolean isTreasure() {
+        return false;
+    }
+
+}
